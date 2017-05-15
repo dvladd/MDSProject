ed89aa6ad14d7d428baa07852ecdb2cecbae3ac6
dvladd <dumitrachevlad11@gmail.com>
2017-05-15 17:04:42 +0300

    First commit, yay!

---
+++ src/mdsproject/MDSProject.java
@@ -0,0 +1,14 @@
+package mdsproject;
+
+/**
+ *
+ * @authors Vlad, Tiberiu, Stefan, Cristi;
+ */
+
+public class MDSProject 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("We started!");
+    }  
+}
