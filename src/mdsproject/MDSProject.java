15f08ef84e9d2f473a4f1f4f1bcf0a4130b56f39
dvladd <dumitrachevlad11@gmail.com>
2017-06-06 20:16:39 +0300

    Added MAIN

--- src/mdsproject/MDSProject.java
+++ src/mdsproject/MDSProject.java
@@ -1,9 +1,13 @@
 package mdsproject;
 
+import java.sql.SQLException;
+
 public class MDSProject {
 
-    public static void main(String[] args) {
-        System.out.println("Test");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException 
+    {
+       FirstWindow frame1 = new FirstWindow();
+       frame1.setVisible(true);
     }
     
 }
